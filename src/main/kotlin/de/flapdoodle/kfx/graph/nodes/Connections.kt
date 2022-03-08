004075f1421ffbb0729763718bb4976eb4ba61c9
Michael Mosmann <michael@mosmann.de>
2022-03-08 18:31:50 +0100

    works somehow

--- src/main/kotlin/de/flapdoodle/kfx/graph/nodes/Connections.kt
+++ src/main/kotlin/de/flapdoodle/kfx/graph/nodes/Connections.kt
@@ -2,40 +2,42 @@ package de.flapdoodle.kfx.graph.nodes
 
 import de.flapdoodle.kfx.events.SharedEventLock
 import de.flapdoodle.kfx.extensions.*
+import de.flapdoodle.kfx.types.AngleAtPoint2D
+import javafx.beans.property.ReadOnlyObjectWrapper
 import javafx.event.EventHandler
 import javafx.geometry.Point2D
 import javafx.scene.Group
 import javafx.scene.input.MouseEvent
+import javafx.scene.paint.Color
 
 class Connections(
     val sharedEventLock: SharedEventLock = SharedEventLock()
 ) : Group() {
 
     private val eventHandler = EventHandler<MouseEvent> { event ->
+        val connector = (event.source as Connector)
+        when (connector.constraint[ConnectorType::class]) {
+            ConnectorType.Plug -> handlePlug(event, connector)
+            ConnectorType.Socket -> handleSocket(event, connector)
+        }
+    }
+
+    private fun handlePlug(event: MouseEvent, connector: Connector) {
         when (event.eventType) {
-//            MouseEvent.MOUSE_CLICKED -> sharedEventLock.ifUnlocked {
-//                event.consume()
-//                rotateConnector(event.source as Connector)
-//            }
-            MouseEvent.MOUSE_PRESSED -> {
-                val connector = (event.source as Connector)
-                if (connector.constraint[ConnectorType::class] == ConnectorType.Plug) {
-                    sharedEventLock.lock(this) {
-                        event.consume()
-                        Action.Move(
-                            clickPosition = event.screenPosition,
-                            layoutPosition = connector.layoutPosition
-                        )
-                    }
-                }
+            MouseEvent.MOUSE_PRESSED -> sharedEventLock.lock(this) {
+                event.consume()
+                Action.Move(
+                    clickPosition = event.screenPosition,
+                    layoutPosition = connector.layoutPosition
+                )
             }
             MouseEvent.MOUSE_DRAGGED -> sharedEventLock.ifLocked(this, Action::class.java) {
                 when (it) {
                     is Action.Move -> {
-                        val connector = (event.source as Connector)
+                        event.consume()
+
                         val diff = event.screenPosition - it.clickPosition
                         connector.layoutPosition = it.layoutPosition + screenDeltaToLocal(diff)
-                        event.consume()
                     }
                 }
             }
@@ -44,6 +46,50 @@ class Connections(
             }
         }
     }
+
+    private fun handleSocket(event: MouseEvent, connector: Connector) {
+        when (event.eventType) {
+            MouseEvent.MOUSE_PRESSED -> sharedEventLock.lock(this) {
+                event.consume()
+                Action.Connect(
+                    clickPosition = event.screenPosition,
+                    layoutPosition = connector.layoutPosition,
+                    source = connector,
+                    destination = ReadOnlyObjectWrapper(AngleAtPoint2D(connector.layoutPosition, 0.0))
+                ).also {
+                    children.addAll(it.curve)
+                }
+            }
+            MouseEvent.MOUSE_DRAGGED -> sharedEventLock.ifLocked(this, Action::class.java) {
+                when (it) {
+                    is Action.Connect -> {
+                        event.consume()
+
+                        val diff = event.screenPosition - it.clickPosition
+                        val currentPos = it.layoutPosition + screenDeltaToLocal(diff)
+
+                        val openSocket = //                            .filter { it.constraint[ConnectorType::class] == ConnectorType.Socket }
+                            children.filtered { it is Connector }
+                                .map { it as Connector }
+                                .filter { it !== connector }
+                                .firstOrNull { it.boundsInParent.intersects(currentPos.x, currentPos.y, 2.0, 2.0) }
+
+                        it.destination.value = (if (openSocket != null) openSocket.connectionPoint()
+                        else AngleAtPoint2D(currentPos, 0.0))
+                    }
+                }
+            }
+            MouseEvent.MOUSE_RELEASED -> sharedEventLock.release(this, Action::class.java) {
+                event.consume()
+                when (it) {
+                    is Action.Connect -> {
+                        children.remove(it.curve)
+                    }
+                }
+            }
+        }
+    }
+
 
     private fun rotateConnector(source: Connector) {
         source.angle(source.angle() + 15.0)
@@ -63,6 +109,19 @@ class Connections(
             val clickPosition: Point2D,
             val layoutPosition: Point2D
         ) : Action()
+
+        data class Connect(
+            val clickPosition: Point2D,
+            val layoutPosition: Point2D,
+            val source: Connector,
+            val destination: ReadOnlyObjectWrapper<AngleAtPoint2D>
+        ) : Action() {
+            val curve = Curves.cubicCurve(source.connectionPointProperty(), destination).also {
+                it.stroke = Color.RED
+                it.fill = Color.TRANSPARENT
+                it.strokeWidth = 1.0
+            }
+        }
     }
 
     enum class ConnectorType {

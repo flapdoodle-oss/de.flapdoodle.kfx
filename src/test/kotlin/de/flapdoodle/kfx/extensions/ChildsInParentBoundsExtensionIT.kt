2407b8c87d62350c6a0bc7f1fbae29afdd830710
Michael Mosmann <michael@mosmann.de>
2022-04-18 08:53:34 +0200

    child bounds

--- src/test/kotlin/de/flapdoodle/kfx/extensions/ChildsInParentBoundsExtensionIT.kt
+++ src/test/kotlin/de/flapdoodle/kfx/extensions/ChildsInParentBoundsExtensionIT.kt
@@ -6,9 +6,7 @@ import javafx.scene.Scene
 import javafx.scene.layout.Pane
 import javafx.scene.shape.Rectangle
 import javafx.stage.Stage
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.ThrowingConsumer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.testfx.api.FxRobot
@@ -30,6 +28,8 @@ internal class ChildsInParentBoundsExtensionIT {
     @Test
     fun rectangleInMovedPaneCenteredAt00(robot: FxRobot) {
         val root = robot.lookup(".root").queryAs(Pane::class.java)
+        root.markAsContainer()
+        
         val movedPane = Pane().apply {
             layoutX = 20.0
             layoutY = 20.0
@@ -59,6 +59,62 @@ internal class ChildsInParentBoundsExtensionIT {
         assertThat(movedPane.containerlessBoundsInLocal())
             .hasBounds(LayoutBounds(5.0, 5.0, 30.0, 30.0))
         assertThat(movedPane.containerlessBoundsInParent())
+            .hasBounds(LayoutBounds(5.0, 5.0, 30.0, 30.0))
+
+        assertThat(root.containerlessBoundsInLocal())
+            .hasBounds(LayoutBounds(5.0, 5.0, 30.0, 30.0))
+        assertThat(root.containerlessBoundsInParent())
+            .hasBounds(LayoutBounds(5.0, 5.0, 30.0, 30.0))
+    }
+
+    @Test
+    fun rectangleIn2MovedPanesCenteredAt00(robot: FxRobot) {
+        val root = robot.lookup(".root").queryAs(Pane::class.java)
+        root.markAsContainer()
+
+        val movedDownPane = Pane().apply {
+            layoutX = 40.0
+            layoutY = 40.0
+            prefWidth = 50.0
+            prefHeight = 50.0
+            markAsContainer()
+        }
+
+        val movedUpPane = Pane().apply {
+            layoutX = -20.0
+            layoutY = -20.0
+            prefWidth = 50.0
+            prefHeight = 50.0
+            markAsContainer()
+        }
+
+        val rectangle = Rectangle(30.0, 30.0).apply {
+            layoutX = -15.0
+            layoutY = -15.0
+        }
+
+        robot.interact {
+            root.children.add(movedDownPane)
+            movedDownPane.children.add(movedUpPane)
+            movedUpPane.children.add(rectangle)
+        }
+
+        assertThat(root.boundsInLocal)
+            .hasBounds(LayoutBounds(0.0, 0.0, 200.0, 200.0))
+
+        assertThat(rectangle.containerlessBoundsInLocal())
+            .hasBounds(LayoutBounds(0.0, 0.0, 30.0, 30.0))
+        assertThat(rectangle.containerlessBoundsInParent())
+            .hasBounds(LayoutBounds(-15.0, -15.0, 30.0, 30.0))
+
+        assertThat(movedUpPane.containerlessBoundsInLocal())
+            .hasBounds(LayoutBounds(-35.0, -35.0, 30.0, 30.0))
+        assertThat(movedUpPane.containerlessBoundsInParent())
+            .hasBounds(LayoutBounds(-35.0, -35.0, 30.0, 30.0))
+
+        assertThat(movedDownPane.containerlessBoundsInLocal())
+            .hasBounds(LayoutBounds(5.0, 5.0, 30.0, 30.0))
+        assertThat(movedDownPane.containerlessBoundsInParent())
             .hasBounds(LayoutBounds(5.0, 5.0, 30.0, 30.0))
 
         assertThat(root.containerlessBoundsInLocal())

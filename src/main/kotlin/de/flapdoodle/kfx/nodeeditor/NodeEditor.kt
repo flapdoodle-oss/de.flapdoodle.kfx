f7bb245a48b1d8b58fe70ae3127e180bf9bd9c9a
Michael Mosmann <michael@mosmann.de>
2023-04-11 21:05:46 +0200

    works somehow

--- src/main/kotlin/de/flapdoodle/kfx/nodeeditor/NodeEditor.kt
+++ src/main/kotlin/de/flapdoodle/kfx/nodeeditor/NodeEditor.kt
@@ -17,10 +17,10 @@ class NodeEditor : AnchorPane() {
   init {
     children.add(NodeView(sharedLock).withAnchors(all = 0.0).apply {
       getViewChildren().addAll(Node("one").apply {
-          layoutPosition = Point2D(100.0, 50.0)
-        },Node("two"))
-      })
-    
+        layoutPosition = Point2D(100.0, 50.0)
+      }, Node("two"))
+    })
+
     addEventHandler(MouseEvent.ANY, this::handleMouseEvent)
   }
 
@@ -29,21 +29,38 @@ class NodeEditor : AnchorPane() {
 //      println("--> ${event.eventType}  ${event.target}")
 //    }
     val target = event.target
-    if (target is Node) {
-      when (event.eventType) {
-        MouseEvent.MOUSE_ENTERED_TARGET -> {
+    when (event.eventType) {
+      MouseEvent.MOUSE_ENTERED_TARGET -> {
+        if (target is Node) {
           sharedLock.tryLock(target) {
             event.consume()
             Node.Style.Active.enable(target)
 
             Action.Focus()
           }
+        } else {
+          sharedLock.ifLocked(Node::class.java, Action.Focus::class.java) { active: Node, action: Action.Focus ->
+            if (target is javafx.scene.Node && Markers.isDragBar(target)) {
+              cursor = SizeMode.INSIDE.cursor()
+              replaceLock(Action.Focus(SizeMode.INSIDE))
+            }
+          }
         }
-        MouseEvent.MOUSE_EXITED_TARGET -> {
+      }
+
+      MouseEvent.MOUSE_EXITED_TARGET -> {
+        if (target is Node) {
           sharedLock.tryRelease(target, Action.Focus::class.java) {
             event.consume()
             cursor = null
             Node.Style.Active.disable(target)
+          }
+        } else {
+          sharedLock.ifLocked(Node::class.java, Action.Focus::class.java) { active: Node, action: Action.Focus ->
+            if (target is javafx.scene.Node && Markers.isDragBar(target)) {
+              cursor = null
+              replaceLock(Action.Focus())
+            }
           }
         }
       }
@@ -51,7 +68,7 @@ class NodeEditor : AnchorPane() {
 
     sharedLock.ifLocked(Node::class.java, Action::class.java) { active: Node, action: Action ->
       if (action is Action.Focus) {
-        println("action -> $action")
+//        println("action -> $action")
         when (event.eventType) {
           MouseEvent.MOUSE_PRESSED -> {
             event.consume()
@@ -59,55 +76,49 @@ class NodeEditor : AnchorPane() {
             val targetLocalPosition = active.screenToLocal(event.screenPosition)
             val sizeMode = SizeMode.guess(targetLocalPosition, active.size)
 
-//            sharedLock.replaceLock(active, Action::class.java) {
-              val newAction = if (sizeMode != null && sizeMode != SizeMode.INSIDE /*&& active.isResizeable()*/) {
-                cursor = sizeMode.cursor()
-                Action.Resize(
-                  clickPosition = event.screenPosition,
-                  sizeMode = sizeMode,
-                  layout = LayoutBounds(active.layoutPosition, active.size)
-                )
-              } else {
-                cursor = SizeMode.INSIDE.cursor()
-                Action.Move(
-                  clickPosition = event.screenPosition,
-                  layoutPosition = active.layoutPosition
-                )
-              }
-              println("replace with $newAction")
-              newAction
+            val newAction = if (sizeMode != null && sizeMode != SizeMode.INSIDE /*&& active.isResizeable()*/) {
+              cursor = sizeMode.cursor()
+              Action.Resize(
+                clickPosition = event.screenPosition,
+                sizeMode = sizeMode,
+                layout = LayoutBounds(active.layoutPosition, active.size)
+              )
+            } else {
+              cursor = SizeMode.INSIDE.cursor()
+              Action.Move(
+                clickPosition = event.screenPosition,
+                layoutPosition = active.layoutPosition
+              )
+            }
+            println("replace with $newAction")
+            newAction
 
-              replaceLock(newAction)
-//            }
+            replaceLock(newAction)
           }
+
           MouseEvent.MOUSE_MOVED -> {
-//            val targetLocalPosition = active.screenToLocal(event.screenPosition)
-//            val sizeMode = SizeMode.guess(targetLocalPosition, active.size)
-//            if (sizeMode != null && sizeMode != SizeMode.INSIDE /*&& active.isResizeable()*/) {
-//              cursor = sizeMode.cursor()
-//            } else {
-//              cursor = SizeMode.INSIDE.cursor()
-//            }
-
-//            sharedLock.replaceLock(active, Action.Focus::class.java) { current ->
-              val replacement: Action.Focus = if (target is javafx.scene.Node && Markers.isDragBar(target)) {
-                Action.Focus(SizeMode.INSIDE)
-              } else {
-                val targetLocalPosition = active.screenToLocal(event.screenPosition)
-                val sizeMode = SizeMode.guess(targetLocalPosition, active.size)
-                if (sizeMode!=SizeMode.INSIDE) {
-                  Action.Focus(sizeMode)
-                } else {
-                  action
-                }
+            val targetLocalPosition = active.screenToLocal(event.screenPosition)
+            val sizeMode = SizeMode.guess(targetLocalPosition, active.size)
+            if (sizeMode != SizeMode.INSIDE) {
+              if (sizeMode!=null) {
+                cursor = sizeMode.cursor()
+                replaceLock(Action.Focus(sizeMode))
               }
-              if (replacement.sizeMode!=null) {
-                cursor=replacement.sizeMode.cursor()
-              } else {
-                cursor=null
+            } else {
+              if (action.sizeMode!=SizeMode.INSIDE) {
+                cursor = null
               }
-
-              replaceLock(replacement)
+            }
+//
+//            if (sizeMode!=SizeMode.INSIDE && action.sizeMode==null) {
+//              val replacement = Action.Focus(sizeMode)
+//              if (sizeMode != null) {
+//                cursor = sizeMode.cursor()
+//              } else {
+//                cursor = null
+//              }
+//
+//              replaceLock(replacement)
 //            }
           }
         }
@@ -127,11 +138,6 @@ class NodeEditor : AnchorPane() {
                 val fixedDiff = active.screenDeltaToLocal(diff)
                 val resizedBounds = SizeMode.resize(action.sizeMode, action.layout, fixedDiff)
 
-//                val resized = active.resizeTo(resizedBounds.size.width, resizedBounds.size.height)
-//                val fix = resized- resizedBounds.size
-//                println("fix -> $fix")
-//                active.layoutPosition = resizedBounds.layoutPosition - Point2D(fix.width, fix.height)
-
                 active.resizeTo(resizedBounds)
               }
 
@@ -142,12 +148,10 @@ class NodeEditor : AnchorPane() {
           }
 
           MouseEvent.MOUSE_RELEASED -> {
-//            sharedLock.replaceLock(active, Action::class.java) { it: Action ->
-              event.consume()
-              cursor = null
+            event.consume()
+            cursor = null
 
-              replaceLock(Action.Focus())
-//            }
+            replaceLock(Action.Focus())
           }
         }
       }
@@ -155,7 +159,7 @@ class NodeEditor : AnchorPane() {
   }
 
   sealed class Action {
-    data class Focus(val sizeMode: SizeMode? = null): Action()
+    data class Focus(val sizeMode: SizeMode? = null) : Action()
     data class Move(
       val clickPosition: Point2D,
       val layoutPosition: Point2D

19ac4c776f894801a01f6cc5d89c96e872b630ee
Michael Mosmann <michael@mosmann.de>
2024-02-12 18:41:50 +0100

    cleanup

--- src/test/kotlin/de/flapdoodle/kfx/sampler/SlimTableSamplerTest.kt
+++ src/test/kotlin/de/flapdoodle/kfx/sampler/SlimTableSamplerTest.kt
@@ -1,7 +1,6 @@
 package de.flapdoodle.kfx.sampler
 
 import javafx.stage.Stage
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith

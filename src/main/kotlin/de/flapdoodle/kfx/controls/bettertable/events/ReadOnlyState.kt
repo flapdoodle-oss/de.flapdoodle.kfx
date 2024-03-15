4122c82ea357b4b844421466c2bb689145584524
Michael Mosmann <michael@mosmann.de>
2024-03-16 00:02:48 +0100

    resize column event

--- src/main/kotlin/de/flapdoodle/kfx/controls/bettertable/events/ReadOnlyState.kt
+++ src/main/kotlin/de/flapdoodle/kfx/controls/bettertable/events/ReadOnlyState.kt
@@ -2,6 +2,14 @@ package de.flapdoodle.kfx.controls.bettertable.events
 
 class ReadOnlyState<T: Any> : State<T> {
     override fun onEvent(event: TableEvent.RequestEvent<T>): State<T> {
+        when (event) {
+            is TableEvent.ResizeColumn<T, out Any> -> {
+                println("TODO: not implemented right now: $event")
+            }
+            else -> {
+                // do nothing
+            }
+        }
         return this
     }
 }

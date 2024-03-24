e111fb42e51e1da289ba226168adcecfe4c8b7a9
Michael Mosmann <michael@mosmann.de>
2024-03-24 16:37:15 +0100

    control 2 region

--- src/main/kotlin/de/flapdoodle/kfx/controls/bettertable/Footer.kt
+++ src/main/kotlin/de/flapdoodle/kfx/controls/bettertable/Footer.kt
@@ -2,38 +2,28 @@ package de.flapdoodle.kfx.controls.bettertable
 
 import de.flapdoodle.kfx.bindings.ObservableLists
 import de.flapdoodle.kfx.extensions.cssClassName
+import de.flapdoodle.kfx.layout.StackLikeRegion
 import javafx.beans.property.ReadOnlyObjectProperty
 import javafx.beans.value.ObservableValue
-import javafx.scene.control.Control
-import javafx.scene.control.SkinBase
 import javafx.scene.layout.HBox
 
 class Footer<T : Any>(
   private val columns: ReadOnlyObjectProperty<List<Column<T, out Any>>>,
   private val columnWidthProperties: (Column<T, out Any>) -> ObservableValue<Number>,
   private val footerColumnFactory: FooterColumnFactory<T>?
-) : Control() {
+) : StackLikeRegion() {
 
-  private val skin = Skin(this)
+  private val footer = HBox()
 
   init {
     isFocusTraversable = false
     cssClassName("footer")
-  }
 
-  override fun createDefaultSkin() = skin
-
-  inner class Skin<T : Any>(
-    private val src: Footer<T>
-  ) : SkinBase<Footer<T>>(src) {
-    private val footer = HBox()
-    init {
-      ObservableLists.syncWith(src.columns, footer.children) {
-        val footerColumn = src.footerColumnFactory?.footerColumn(it) ?: FooterColumn(it)
-        footerColumn.prefWidthProperty().bind(src.columnWidthProperties(it))
-        footerColumn
-      }
-      children.add(footer)
+    ObservableLists.syncWith(columns, footer.children) {
+      val footerColumn = footerColumnFactory?.footerColumn(it) ?: FooterColumn(it)
+      footerColumn.prefWidthProperty().bind(columnWidthProperties(it))
+      footerColumn
     }
+    children.add(footer)
   }
 }

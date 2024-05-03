f3d25247d82151103773917b6d985889f3865b1e
Michael Mosmann <michael@mosmann.de>
2024-05-03 22:07:41 +0200

    ticks labels working

--- src/main/kotlin/de/flapdoodle/kfx/controls/charts/parts/TicksLabelPane.kt
+++ src/main/kotlin/de/flapdoodle/kfx/controls/charts/parts/TicksLabelPane.kt
@@ -11,13 +11,8 @@ import de.flapdoodle.kfx.types.BoundingBoxes
 import de.flapdoodle.kfx.types.CardinalDirection
 import de.flapdoodle.kfx.types.Direction
 import javafx.beans.value.ObservableValue
-import javafx.geometry.HPos
-import javafx.geometry.Point2D
-import javafx.geometry.VPos
-import javafx.scene.layout.Background
+import javafx.geometry.*
 import javafx.scene.layout.Pane
-import javafx.scene.paint.Color
-import kotlin.math.max
 import kotlin.math.min
 
 class TicksLabelPane<T : Any>(
@@ -36,16 +31,14 @@ class TicksLabelPane<T : Any>(
   }
 
   override fun computePrefHeight(width: Double): Double {
-    return children.map { it.prefHeight(width) }.maxOrNull() ?: (0.0 + insets.top + insets.bottom)
+    return children.maxOfOrNull { it.prefHeight(width) } ?: (0.0 + insets.top + insets.bottom)
   }
 
   override fun computePrefWidth(height: Double): Double {
-    return children.map { it.prefWidth(width) }.maxOrNull() ?: (0.0 + insets.left + insets.right)
+    return children.maxOfOrNull { it.prefWidth(width) } ?: (0.0 + insets.left + insets.right)
   }
 
   override fun layoutChildren() {
-//    println("layout: ${layoutBounds}")
-
     val scaleLength = when (direction) {
       Direction.BOTTOM, Direction.TOP -> width - insets.left - insets.right
       Direction.LEFT, Direction.RIGHT -> height - insets.top - insets.bottom
@@ -57,6 +50,8 @@ class TicksLabelPane<T : Any>(
     }
 
     val r = range.value
+
+    var label2position = emptyList<TickedLabelAndBounds<T>>()
 
     children.forEach { node ->
       if (node.isManaged) {
@@ -83,12 +78,14 @@ class TicksLabelPane<T : Any>(
           val w = snappedToPixel(min(prefWidth, width - insets.left - insets.right))
           val h = snappedToPixel(min(prefHeight, height - insets.top - insets.bottom))
 
-          val delta = BoundingBoxes.pointAtEdge(ticketLabelBounds, when (direction) {
-            Direction.LEFT -> CardinalDirection.EAST
-            Direction.RIGHT -> CardinalDirection.WEST
-            Direction.TOP -> CardinalDirection.SOUTH
-            Direction.BOTTOM -> CardinalDirection.NORTH
-          }).point2D
+          val delta = BoundingBoxes.pointAtEdge(
+            ticketLabelBounds, when (direction) {
+              Direction.LEFT -> CardinalDirection.EAST
+              Direction.RIGHT -> CardinalDirection.WEST
+              Direction.TOP -> CardinalDirection.SOUTH
+              Direction.BOTTOM -> CardinalDirection.NORTH
+            }
+          ).point2D
 
           if (direction == Direction.TOP) {
 //            println("--> $scalePoint (h: $height, inset.b: ${insets.bottom})")
@@ -99,6 +96,8 @@ class TicksLabelPane<T : Any>(
           val x = pos.x
           val y = pos.y
 
+          label2position = label2position + (TickedLabelAndBounds(tickedLabel, BoundingBox(x, y, w, h)))
+
           layoutInArea(tickedLabel, x, y, w, h, -1.0, HPos.LEFT, VPos.TOP)
         } else {
           if (node.isResizable) {
@@ -107,13 +106,37 @@ class TicksLabelPane<T : Any>(
         }
       }
     }
+
+    val sorted = label2position.sortedBy { it.bounds.minX }.sortedBy { it.bounds.minY }
+
+    val labelsWithoutCollisions = filterUntilNoCollisions(sorted).map { it.tickLabel }.toSet()
+
+    sorted.forEach {
+      it.tickLabel.isVisible = labelsWithoutCollisions.contains(it.tickLabel)
+    }
+  }
+
+  private fun filterUntilNoCollisions(list: List<TickedLabelAndBounds<T>>): List<TickedLabelAndBounds<T>> {
+    val odd = list.filterIndexed { index, pair -> index % 2 == 0 }
+    val even = list.filterIndexed { index, pair -> index % 2 == 1  }
+    val collides = odd.zip(even).any { (e, o) -> e.bounds.intersects(o.bounds) }
+    return if (collides) {
+       filterUntilNoCollisions(odd)
+    } else {
+      list
+    }
   }
 
   private fun snappedToPixel(value: Double): Double {
     return if (isSnapToPixel) ScaledMath.ceil(value, 1.0) else value
   }
 
-  class TickLabel<T: Any>(
+  data class TickedLabelAndBounds<T: Any>(
+    val tickLabel: TickLabel<T>,
+    val bounds: Bounds
+  )
+
+  class TickLabel<T : Any>(
     converter: ValidatingConverter<T>,
     val tick: T,
   ) : ValidatedLabel<T>(converter) {

3d7bf8831b74807474e07a2e830835d8f2a0760f
Michael Mosmann <michael@mosmann.de>
2024-05-23 00:17:16 +0200

    hack works

--- src/main/kotlin/de/flapdoodle/kfx/controls/fields/ValidatingColoredTextField.kt
+++ src/main/kotlin/de/flapdoodle/kfx/controls/fields/ValidatingColoredTextField.kt
@@ -7,12 +7,17 @@ import de.flapdoodle.kfx.converters.ValidatingConverter
 import de.flapdoodle.kfx.extensions.bindCss
 import de.flapdoodle.kfx.extensions.cssClassName
 import javafx.beans.property.SimpleObjectProperty
+import javafx.geometry.BoundingBox
+import javafx.geometry.Bounds
 import javafx.geometry.Pos
+import javafx.scene.Node
+import javafx.scene.Parent
 import javafx.scene.control.TextField
 import javafx.scene.control.Tooltip
 import javafx.scene.layout.Border
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
+import javafx.scene.text.Text
 
 class ValidatingColoredTextField<T: Any>(
   val converter: ValidatingConverter<T>,
@@ -35,6 +40,7 @@ class ValidatingColoredTextField<T: Any>(
   private val colors = SimpleObjectProperty<List<ColoredLabel.Part>>(emptyList())
   private val coloredLabel = ColoredLabel(delegate.textProperty(), colors).apply {
     cssClassName("colored-label")
+    isManaged = false
 //    borderProperty().bind(delegate.borderProperty())
   }
 
@@ -51,30 +57,41 @@ class ValidatingColoredTextField<T: Any>(
 
     setAlignment(delegate, Pos.TOP_LEFT)
     setAlignment(coloredLabel, Pos.CENTER)
-    
-    children.addAll(delegate, coloredLabel)
 
-//    delegate.focusedProperty().addListener { observable, oldValue, newValue ->
-//      if (!newValue) {
-//        delegate.opacity = 0.0
-//        coloredLabel.opacity = 1.0
-//      } else {
-//        delegate.opacity = 1.0
-//        coloredLabel.opacity = 0.0
-//      }
-//    }
+    children.addAll(delegate, coloredLabel)
 
     coloredLabel.isFocusTraversable = false
     coloredLabel.isMouseTransparent = true
     coloredLabel.opacity = 0.9
-//    coloredLabel.focusedProperty().addListener { observable, oldValue, newValue ->
-//      if (newValue) {
-//        delegate.requestFocus()
-//      }
-//    }
-//    coloredLabel.addEventFilter(MouseEvent.MOUSE_CLICKED) { event ->
-//      delegate.requestFocus()
-//    }
+
+
+    // HACK
+    delegate.skinProperty().addListener { _, _, s ->
+      val f: Node? = delegate.childrenUnmodifiable.firstOrNull()
+      if (f!=null && f is Parent) {
+        val t = f.childrenUnmodifiable.find { it is Text } as Text?
+        println("--> $t")
+        if (t != null) {
+//          coloredLabel.layoutYProperty().bind(t.layoutYProperty())
+//          coloredLabel.translateXProperty().bind(t.layoutXProperty())
+          val bbox = ObjectBindings.merge(f.boundsInParentProperty(), t.boundsInParentProperty()) { pb, tb ->
+            BoundingBox(tb.minX + pb.minX, tb.minY + pb.minY, tb.width, tb.height)
+          }
+          bbox.addListener { _, _, n ->
+            coloredLabel.resizeRelocate(n.minX, n.minY, n.width, n.height)
+          }
+//          t.boundsInParentProperty().addListener { _, _, n ->
+//            coloredLabel.resizeRelocate(n.minX, n.minY, n.width, n.height)
+//          }
+//          t.layoutBoundsProperty().addListener { observable, oldValue, newValue ->
+//
+////            coloredLabel.resizeRelocate(newValue.minX, newValue.minY, newValue.width, newValue.height)
+//            coloredLabel.resize(newValue.width, newValue.height)
+//          }
+//          coloredLabel.layoutYProperty().bind(t.layoutYProperty())
+        }
+      }
+    }
   }
 
   override fun get() = delegate.get()

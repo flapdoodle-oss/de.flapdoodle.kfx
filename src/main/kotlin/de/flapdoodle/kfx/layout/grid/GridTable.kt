4f572deada9c92ecc718d7d98b5086ae59d2cb8d
Michael Mosmann <michael@mosmann.de>
2024-07-25 09:52:52 +0200

    grid table

---
+++ src/main/kotlin/de/flapdoodle/kfx/layout/grid/GridTable.kt
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2022
+ *   Michael Mosmann <michael@mosmann.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.flapdoodle.kfx.layout.grid
+
+import de.flapdoodle.kfx.collections.Diff
+import de.flapdoodle.kfx.extensions.withAnchors
+import javafx.beans.property.ReadOnlyObjectProperty
+import javafx.geometry.HPos
+import javafx.geometry.VPos
+import javafx.scene.Node
+import javafx.scene.layout.AnchorPane
+
+class GridTable<T : Any, I : Any>(
+  model: ReadOnlyObjectProperty<List<T>>,
+  private val indexOf: (T) -> I,
+  private val columns: List<Column<T>>,
+  private val headerFactory: HeaderFooterNodeFactory<T>? = null,
+  private val footerFactory: HeaderFooterNodeFactory<T>? = null,
+) : AnchorPane() {
+  init {
+    require(columns.toSet().size == columns.size) { "some columns are added more than once: $columns" }
+  }
+
+  private val grid = GridPane()
+
+  private var headerNodes: Map<Column<T>, Node>? = null
+  private var rows = emptyList<Row<T, I>>()
+  private var footerNodes: Map<Column<T>, Node>? = null
+
+  fun verticalSpace() = grid.verticalSpace
+  fun horizontalSpace() = grid.horizontalSpace
+
+  init {
+    grid.withAnchors(all = 0.0)
+    children.add(grid)
+
+    update(emptyList(), model.value)
+    model.addListener { _, oldValue, newValue ->
+      update(oldValue, newValue)
+    }
+  }
+
+  private fun update(old: List<T>, new: List<T>) {
+    val newHeaderNodes = headerFactory?.nodesOf(new, columns)
+    val newFooterNodes = footerFactory?.nodesOf(new, columns)
+
+//    require(newHeaderNodes==null || newHeaderNodes.size == columns.size) {"headerNodes size does not match column size"}
+//    require(newFooterNodes==null || newFooterNodes.size == columns.size) {"footerNodes size does not match column size"}
+
+    val changes = Diff.between(old, new, indexOf)
+
+    val removedIds = changes.removed.map { indexOf(it) }.toSet()
+    val modified = changes.modified.map { indexOf(it.second) to it.second }.toMap()
+    val addedIds = changes.added.map { indexOf(it) }.toSet()
+
+    val (nodesToRemove, rowsWithoutRemoved) = rows.partition { removedIds.contains(it.index) }
+
+    val newRows = new.filter { addedIds.contains(indexOf(it)) }.map {
+      Row(indexOf(it), columns.map { c ->
+        c.cellFactory?.let { f -> f.cellOf(it) }
+      })
+    }
+
+    val allRows = rowsWithoutRemoved + newRows
+    val offset = if (newHeaderNodes != null) 1 else 0
+
+    allRows.forEachIndexed { rowIndex, row ->
+      row.nodes.forEachIndexed { columnIndex, nodeAndUpdate ->
+        if (nodeAndUpdate != null) {
+          val column = columns[columnIndex]
+          GridPane.setPosition(nodeAndUpdate.node, columnIndex, rowIndex + offset, column.horizontalPosition, column.verticalPosition)
+        }
+      }
+    }
+
+    columns.forEachIndexed { index, column ->
+      val header = headerNodes?.get(column)
+      if (header != null) grid.children.removeAll(header)
+    }
+    if (newHeaderNodes != null) {
+      columns.forEachIndexed { index, column ->
+        val header = newHeaderNodes[column]
+        if (header != null) {
+          GridPane.setPosition(header, index, 0, column.horizontalPosition, column.verticalPosition)
+          grid.children.add(header)
+        }
+      }
+    }
+    headerNodes = newHeaderNodes
+
+    nodesToRemove.forEach { row -> row.nodes.forEach { if (it != null) grid.children.removeAll(it.node) } }
+    rowsWithoutRemoved.forEach { row ->
+      val changedValue = modified[row.index]
+      if (changedValue != null) {
+        row.nodes.forEach { it: TableCell<T, out Node>? ->
+          it?.updateCell(changedValue)
+        }
+      }
+    }
+    newRows.forEach { row -> row.nodes.forEach { if (it != null) grid.children.add(it.node) } }
+    rows = allRows
+
+    columns.forEachIndexed { index, column ->
+      val header = footerNodes?.get(column)
+      if (header != null) grid.children.removeAll(header)
+    }
+    if (newFooterNodes != null) {
+      columns.forEachIndexed { index, column ->
+        val footer = newFooterNodes[column]
+        if (footer != null) {
+          GridPane.setPosition(footer, index, allRows.size + offset, column.horizontalPosition, column.verticalPosition)
+          grid.children.add(footer)
+        }
+      }
+    }
+    footerNodes = newFooterNodes
+  }
+
+  fun interface HeaderFooterNodeFactory<T : Any> {
+    fun nodesOf(values: List<T>, columns: List<Column<T>>): Map<Column<T>, Node>
+  }
+  
+  fun interface CellFactory<T: Any, N: Node> {
+    fun cellOf(value: T): TableCell<T, out N>
+  }
+
+  data class Column<T : Any>(
+    val weight: Double = 1.0,
+    val cellFactory: CellFactory<T, out Node>? = null,
+    val horizontalPosition: HPos? = null,
+    val verticalPosition: VPos? = null
+  )
+
+  private data class Row<T : Any, I : Any>(
+    val index: I,
+    val nodes: List<TableCell<T, out Node>?>
+  )
+}
---
+++ src/test/kotlin/de/flapdoodle/kfx/layout/grid/GridTableSampler.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2022
+ *   Michael Mosmann <michael@mosmann.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.flapdoodle.kfx.layout.grid
+
+import de.flapdoodle.kfx.controls.fields.TypedTextField
+import de.flapdoodle.kfx.layout.grid.TableCell.Companion.with
+import de.flapdoodle.kfx.types.Id
+import javafx.application.Application
+import javafx.beans.property.SimpleObjectProperty
+import javafx.event.EventHandler
+import javafx.geometry.HPos
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.stage.Stage
+
+class GridTableSampler {
+  class Sample : Application() {
+
+    override fun start(stage: Stage) {
+
+      val model = SimpleObjectProperty(
+        listOf<Person>(
+          Person("Anna", 24),
+          Person("Peter", 19),
+          Person("Susi", 21)
+        )                             
+      )
+
+      model.addListener { observable, oldValue, newValue ->
+        println("changed: $newValue")
+      }
+
+      val nameColumn = GridTable.Column<Person>(cellFactory = { TableCell(Label(it.name)) }, horizontalPosition = HPos.LEFT)
+      val ageColumn = GridTable.Column<Person>(weight = 2.0, cellFactory = {
+        val textField = TypedTextField(Int::class).apply {
+          set(it.age)
+          valueProperty().addListener { observable, oldValue, newValue ->
+            println("value changed: ${it.id} -> ${get()}")
+            model.value = model.value.map { p -> if (p.id == it.id) it.copy(age = get()) else p }
+          }
+        }
+        TableCell.initializedWith(it)
+          .node(textField)
+          .map(Person::age)
+          .updateWith(TypedTextField<Int>::set)
+        with(textField).map(Person::age).updateWith(TypedTextField<Int>::set)
+      })
+      val actionColumn = GridTable.Column<Person>(weight = 1.0, cellFactory = { t ->
+        val button = Button("-").apply {
+          onAction = EventHandler {
+            model.value = model.value.filter { p -> p.id != t.id }
+          }
+        }
+        TableCell(button)
+      })
+
+      val columns = listOf(
+        nameColumn,
+        ageColumn,
+        actionColumn
+      )
+      stage.scene = Scene(GridTable(
+        model = model,
+        indexOf = Person::id,
+        columns = columns,
+        headerFactory = { values, _ ->
+          mapOf(nameColumn to Label("Name"), ageColumn to Label("Age"))
+        },
+        footerFactory = { values, _ ->
+          val name = TextField("")
+          val age = TypedTextField(Int::class)
+          val add = Button("+").apply {
+            onAction = EventHandler {
+              if (name.text != null && name.text.isNotBlank()) {
+                model.value = model.value + Person(name.text, age.get())
+              }
+            }
+          }
+          mapOf(nameColumn to name, ageColumn to age, actionColumn to add)
+        }
+      ).apply {
+        verticalSpace().set(5.0)
+        horizontalSpace().set(10.0)
+      })
+      stage.show()
+    }
+  }
+
+  data class Person(val name: String, val age: Int?, val id: Id<Person> = Id.nextId(Person::class))
+
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      Application.launch(Sample::class.java, *args)
+    }
+  }
+}

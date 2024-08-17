0a70f6b45f4c0729ec90853f6e325f6d59b85bbe
Michael Mosmann <michael@mosmann.de>
2024-08-17 18:26:13 +0200

    in progress

--- src/main/kotlin/de/flapdoodle/kfx/controls/fields/ValidatingComboBox.kt
+++ src/main/kotlin/de/flapdoodle/kfx/controls/fields/ValidatingComboBox.kt
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections
 import javafx.collections.transformation.FilteredList
 import javafx.scene.control.ComboBox
 import javafx.util.StringConverter
+import java.util.function.Predicate
 
 class ValidatingComboBox<T : Any>(
   val values: List<T?>,
@@ -14,7 +15,7 @@ class ValidatingComboBox<T : Any>(
 
   private val all = FXCollections.observableArrayList(values)
   private val filtered = FilteredList(all)
-  private var state: State<T> = Start()
+  private var state: State<T> = Inactive()
 
   init {
     isEditable = true
@@ -36,11 +37,11 @@ class ValidatingComboBox<T : Any>(
     }
 
     selectionModel.selectedItemProperty().addListener { _, _, newValue ->
-      state = state.onEvent(UIChange.SelectionChange(newValue))
+      state = state.onEvent(UIChange.SelectionChange(newValue)).apply(this)
     }
 
     showingProperty().addListener { _, _, isShowing ->
-      state = state.onEvent(if (isShowing) UIChange.MenuShowing() else UIChange.MenuHiding())
+      state = state.onEvent(if (isShowing) UIChange.MenuShowing() else UIChange.MenuHiding()).apply(this)
 //      println("showing: $isShowing")
 //      if (!isShowing) {
 //        println("selection: ${selectionModel.selectedItem}")
@@ -49,11 +50,11 @@ class ValidatingComboBox<T : Any>(
     }
 
     editor.focusedProperty().addListener { _, _, newValue ->
-      state = state.onEvent(if (newValue) UIChange.EditorFocused() else UIChange.EditorBlured())
+      state = state.onEvent(if (newValue) UIChange.Focused() else UIChange.Blurred()).apply(this)
     }
 
     editor.textProperty().addListener { _, _, newValue ->
-      state = state.onEvent(UIChange.TextChanged(newValue))
+      state = state.onEvent(UIChange.TextChanged(newValue)).apply(this)
 //      println("text: '$newValue'")
 //      if (selectionModel.selectedItem==null && newValue != null && newValue.isNotBlank()) {
 //        println("filter--> '$newValue'")
@@ -66,48 +67,83 @@ class ValidatingComboBox<T : Any>(
   }
 
   private interface State<T: Any> {
-    fun onEvent(event: UIChange<T>): State<T>
+    fun onEvent(event: UIChange<T>): StateChange<T>
+  }
+
+  private fun interface Action<T: Any> {
+    fun onAction(node: ValidatingComboBox<T>)
+  }
+
+  private data class StateChange<T: Any>(
+    val next: State<T>,
+    val actions: List<Action<T>> = emptyList()
+  ) {
+    constructor(next: State<T>, vararg actions: Action<T>) : this(next, listOf(*actions))
+
+    fun apply(node: ValidatingComboBox<T>): State<T> {
+      println("apply actions: $actions")
+      actions.forEach { it.onAction(node) }
+      println("next: $next")
+      return next
+    }
   }
 
   private sealed class UIChange<T: Any> {
-    class EditorFocused<T: Any> : UIChange<T>()
-    class EditorBlured<T: Any> : UIChange<T>()
+    class Focused<T: Any> : UIChange<T>()
+    class Blurred<T: Any> : UIChange<T>()
     data class TextChanged<T: Any>(val text: String) : UIChange<T>()
     class MenuShowing<T: Any>() : UIChange<T>()
     class MenuHiding<T: Any>() : UIChange<T>()
     data class SelectionChange<T: Any>(val selection: T?): UIChange<T>()
   }
 
-  private class Start<T: Any> : State<T> {
-    override fun onEvent(event: UIChange<T>): State<T> {
+  private class Inactive<T: Any> : State<T> {
+    override fun onEvent(event: UIChange<T>): StateChange<T> {
       println("${this.javaClass.simpleName}: $event")
       
       return when (event) {
-        is UIChange.EditorFocused -> Focused()
-        else -> this
+        is UIChange.Focused -> StateChange(Focused())
+        else -> StateChange(this)
       }
     }
 
   }
 
   private class Focused<T: Any> : State<T> {
-    override fun onEvent(event: UIChange<T>): State<T> {
+    override fun onEvent(event: UIChange<T>): StateChange<T> {
       println("${this.javaClass.simpleName}: $event")
 
       return when (event) {
-        is UIChange.EditorBlured -> Start()
-        else -> this
+        is UIChange.Blurred -> StateChange(Inactive())
+
+        is UIChange.TextChanged -> Filtered.filter(event.text)
+        else -> StateChange(this)
       }
     }
   }
 
-  private class ListOpened<T: Any> : State<T> {
-    override fun onEvent(event: UIChange<T>): State<T> {
+  private class Filtered<T: Any> : State<T> {
+    override fun onEvent(event: UIChange<T>): StateChange<T> {
       println("${this.javaClass.simpleName}: $event")
-      return when (event) {
-        else -> this
-      }
+      return StateChange(this)
     }
 
+    companion object {
+      fun <T: Any> filter(text: String): StateChange<T> {
+        return StateChange(Filtered(), SetFilter(text), ShowMenu(true))
+      }
+    }
+  }
+
+  private data class SetFilter<T: Any>(val text: String): Action<T> {
+    override fun onAction(node: ValidatingComboBox<T>) {
+      node.filtered.predicate = Predicate { true }
+    }
+  }
+
+  private data class ShowMenu<T: Any>(val show: Boolean): Action<T> {
+    override fun onAction(node: ValidatingComboBox<T>) {
+      if (show) node.show() else node.hide()
+    }
   }
 }
